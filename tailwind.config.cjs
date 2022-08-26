1fb0635a7a5a045156d77b8bd2bafab2b422af2c
Mattykea <math764n@stud.kea.dk>
2022-08-26 09:01:23 +0200

    commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#293462",
+      secondary: "#FEDB39",
+      accent: "#1CD6CE",
+    },
+
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
